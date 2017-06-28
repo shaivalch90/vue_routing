dea0347f0e32b927fc44915c137cd8bb823225e4
Shaival Chettipally <c.shaival.chettipally@uptake.com>
2017-06-27 22:27:28 -0500

    feat(initial-commit): set up vue project using cli and add basic routing

---
+++ src/Home.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h2>{{ title }}</h2>
+    </div> 
+</template>
+
+<script>
+export default {
+    name: 'app-home',
+    data(){
+        return {
+            title: 'This is home page'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+ h2 {
+     color: green;
+ }
+</style>
+
---
+++ src/User.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h2>{{ title }}</h2>
+    </div> 
+</template>
+
+<script>
+export default {
+    name: 'app-home',
+    data(){
+        return {
+            title: 'This is Users page'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+h2 {
+    color: blue;
+}
+</style>
